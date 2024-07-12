bb3370f343c8663acd94dbc6736a039c3f0208f5
Pranav Rangate <pranavrangate231104@gmail.com>
2024-07-12 20:25:05 +0530

    list  ,  tuple   and     set

---
+++ dt.py
@@ -0,0 +1,14 @@
+print('This is example of list:')
+ls=[10,20,30,40,50]
+print(ls,'\n')
+print('list after addition of a number in 2nd position')
+ls.insert(1,15)
+print(ls,'\n')
+
+print('This is example of tuple:')
+tp=(10,20,30,40,50)
+print(tp,'\n')
+
+print('This is example of set:')
+st={10,20,30,40,50,10,10}
+print(st,'\n')
