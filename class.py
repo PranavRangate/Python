481b45a36157d9d77aa064989ad21e79384c725c
Pranav Rangate. <pranavrangate231104@gmail.com>
2024-07-16 22:06:48 +0530

    Example of Python Class and object

---
+++ class.py
@@ -0,0 +1,15 @@
+
+class Dog:
+
+    attr1 = "mammal"
+    attr2 = "dog"
+
+    def fun(self):
+        print("I'm a", self.attr1)
+        print("I'm a", self.attr2)
+
+
+Rodger = Dog()
+
+print(Rodger.attr1)
+Rodger.fun()
