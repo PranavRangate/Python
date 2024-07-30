a8f2fead0bf9924494af70f2f6edd506f78cf7ce
Pranav Rangate. <pranavrangate231104@gmail.com>
2024-07-30 10:50:21 +0530

    operator overload

--- complex.py
+++ complex.py
@@ -1,2 +1,26 @@
-n1=complex(2,3)
-print(n1)
+class Complex:
+    def __init__(self,real,img):
+        self.real=real
+        self.img=img
+        
+    def showNumb(self):
+        print(self.real,"i + ",self.img,"j")
+        
+    def __add__(self,any_num):
+        new_real=self.real+any_num.real
+        new_img=self.img+any_num.img
+        return Complex(new_real,new_img)
+    
+    def __sub__(self,any_num):
+        new_real=self.real-any_num.real
+        new_img=self.img-any_num.img
+        return Complex(new_real,new_img)
+    
+n1 = Complex(3,4)
+n2 = Complex(1,2)
+
+n3 = n1+n2
+n4 = n1-n2
+
+n3.showNumb()
+n4.showNumb()
