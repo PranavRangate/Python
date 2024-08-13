c905f4ca621037d81c814facf62adf65bbad4bd7
Pranav Rangate. <pranavrangate231104@gmail.com>
2024-08-13 10:57:21 -0700

    data visualization

---
+++ simple_line_plot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[10,15,20,25,30]
+
+plt.plot(x,y)
+
+plt.title('simple line graph')
+plt.xlabel('values of x')
+plt.ylabel('values of y')
+
+plt.show()
