01ee78426c271ebd9fa522cf24fc44748f74e308
Pranav Rangate. <pranavrangate231104@gmail.com>
2024-08-30 07:44:43 -0700

    Add files via upload

---
+++ module.py
@@ -0,0 +1,24 @@
+import math as m
+
+print(m.sqrt(16))
+
+print(m.factorial(6))
+
+print(m.sqrt(25)) 
+
+print(m.pi) 
+
+print(m.degrees(2))  
+
+print(m.radians(60))  
+
+print(m.sin(2))  
+
+print(m.cos(0.5))  
+
+print(m.tan(0.23)) 
+
+print(m.factorial(4))
+
+import time
+print(time.time())
